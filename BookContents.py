c214d4146b19d81bc48117295df5ee0ce3fe7e64
AlexNickKovalenko <39223173+AlexNickKovalenko@users.noreply.github.com>
2018-05-27 15:31:10 -0400

    Initial commit

---
+++ BookContents.py
@@ -0,0 +1,34 @@
+LItem0=dict(num=0,cat="cognition",source='Printed Book',
+            title='Cognition',
+            author='Daniel Riesenberg', location="Bookshelf",row=0,column=0,grade='Popular',
+            contents=['briefContents',['PART_1 The Foundation of Cognitive Psychology',
+            ['1 the Science of the Mind','2 The Neural Basis for Cognition'],
+                                       ['Part_2. LEARNING ABOUT THE WORLD AROUND US',
+                                        '3 Recognizing Objects 75','4 Paying Attention 117'],
+                                       ['Part_3. Memory 161',
+                                        '5 The Acquisition of Memories and the Working Memory System 163',
+                                        '6 Interconnections between Acquisition and Retrieval 199',
+                                        '7 Remembering Complex Events 237'],
+                                       ['Part_4. KNOWLEDGE. 281',
+                                        '8 Concepts and Generic Knowledge 283',
+                                        '9 Language 323',
+                                        '10 Visual Knowledge 363']]],
+                                        notes=['Fifth Edition'])
+            
+if __name__=='__main__':
+     print(LItem0['contents'])
+     print(8*'+')
+     print(LItem0['contents'][1])
+     print(8*'+') 
+     print(LItem0['contents'][1][1][1])
+     print(8*'+')
+     print(LItem0['notes'])
+
+
+
+
+
+
+
+           
+     
